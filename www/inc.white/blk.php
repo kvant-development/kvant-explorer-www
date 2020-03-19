c6ab713423405c2998520c2d624b22b9cf91f4e8
Kvant.Development <development@kvant.io>
2020-03-19 10:39:38 +0000

    add new item file

---
+++ www/inc.white/blk.php
@@ -0,0 +1,14 @@
+<?php
+
+print "item2 = $item2";
+/*
+switch($item2)
+{
+    case "id":
+    break;
+    case "hash":
+    break;
+}
+*/
+//$f = 
+?>
---
+++ www/inc.white/blk_id.php
@@ -0,0 +1,3 @@
+<?php
+include "block.php";
+?>
