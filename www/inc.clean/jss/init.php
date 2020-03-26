c7145f3141d2b6320defb13961f7f92ba1098857
Kvant.Development <development@kvant.io>
2020-03-26 15:59:46 +0000

    init

--- www/inc.clean/jss/init.php
+++ www/inc.clean/jss/init.php
@@ -1,4 +1,12 @@
 <?php
+$api_addr = "https://api.kvant.io/";
 
+
+if($_SERVER['HTTP_HOST']=="explorer2.kvant.io")
+$api_addr = "https://api2.kvant.io/";
+
+$api_addr .= "api/v1/mainnet/";
+
+print "var api_addr = \"$api_addr\";\n";
 ?>
-var api_addr = "https://api.kvant.io/api/v1/mainnet/";
+
